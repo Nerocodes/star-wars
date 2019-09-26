ca9a30a0dca30b38a73177416deb6ce04616d2dd
nerocodes <neropaulej@gmail.com>
2019-09-26 03:55:38 +0100

    feature: added header component added assets

---
+++ src/components/Header/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="header">
+        <div class="logo">
+            <img src="@/assets/img/logo.png" alt="Logo">
+        </div>
+        <div class="centered-text">
+            <div class="heading">
+                <div class="logo">
+                  <img src="@/assets/img/logo.png" alt="Logo">  
+                </div>
+                <p>Directory</p>
+            </div>
+            <p class="description">
+                Find your favorite Characters, Films, Species, Starships and Planets
+            </p>
+            <div class="search">
+                <div class="icon">
+                    <i class="fas fa-search"></i>
+                </div>
+                <input type="text" class="search-field" placeholder="Enter a search term">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+import './header.scss';
+
+export default {
+  name: 'Header'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
 	<div>
+		<Header />
 		<h1>Home</h1>
 	</div>
 </template>
+
+<script>
+import Header from '../components/Header';
+
+export default {
+	components: {
+		Header
+	}
+}
+</script>
